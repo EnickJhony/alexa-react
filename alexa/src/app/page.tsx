72faddd8800b1058a1089ec3e4596b589ee705c6
Enick Jhony <enick.jhony@gmail.com>
2023-05-10 10:32:15 -0400

    incluindo componente Header ao App

--- alexa/src/app/page.tsx
+++ alexa/src/app/page.tsx
@@ -1,37 +1,9 @@
+import { Header } from "@/components/header";
+
 export default function Home() {
   return (
     <body>
-      <header className="max-w-[968px] ml-auto mr-auto mt-6 mb-6">
-        <nav className="flex">
-          <a href="#">Alexa</a>
-          <div className="ml-auto">
-            <ul className="flex gap-8">
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Skills</a>
-              </li>
-              <li>
-                <a href="#">Services</a>
-              </li>
-              <li>
-                <a href="#">Portfolio</a>
-              </li>
-              <li>
-                <a href="#">Contactme</a>
-              </li>
-            </ul>
-            <div></div>
-          </div>
-          <div className="ml-8">
-            <a href="#">Lua</a>
-          </div>
-        </nav>
-      </header>
+      <Header />
       <main></main>
       <footer></footer>
     </body>
